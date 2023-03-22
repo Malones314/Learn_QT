3dad5010607142de2dc50edcfcb025287bf8e162
RuiChenZhai <54008162+Malones314@users.noreply.github.com>
2023-03-22 10:34:19 +0800

    Update widget.cpp

--- chapter_11_MySignalSlot/widget.cpp
+++ chapter_11_MySignalSlot/widget.cpp
@@ -15,33 +15,50 @@ Widget::Widget(QWidget *parent)
   te = new Teacher(this);
 
   //连接信号槽
-  //connect( this->te, &Teacher::hungury, this->st, &Student::treat);
+  //connect( this->te, &Teacher::hungry, this->st, &Student::treat);
 
   //连接有参数的信号槽
   //connect( this->te, &Teacher::hungry, this->st, &Student::treat);  //error，发生了重载不知道调用哪一个
-  //connect(this->tr, SIGNAL(hungry(QString)), this->st, SLOT(treat(QString)));
+  //connect(this->te, SIGNAL(hungry(QString)), this->st, SLOT(treat(QString)));
 
-  void (Teacher:: * teacherSingalNoStr)() = &Teacher::hungury;
-  void (Student:: * studentSlotNoStr)() = &Student::treat;
-  connect(this->te,teacherSingalNoStr,this->st,studentSlotNoStr);
+//  void (Teacher:: * teacherSingal)(QString) = &Teacher::hungry;
+//  void (Student:: * studentSlot)(QString) = &Student::treat;
+//  connect( this->te, teacherSingal, this->st, studentSlot);
 
 
-  void (Teacher:: * teacherSingal)(QString) = &Teacher::hungury;
-  void (Student:: * studentSlot)(QString) = &Student::treat;
-  connect(this->te,teacherSingal,this->st,studentSlot);
+  void (Teacher:: * teacherSingal2)() = &Teacher::hungry;
+  void (Student:: * studentSlot2)() = &Student::treat;
+  connect( this->te, teacherSingal2, this->st, studentSlot2);
+
 
   QPushButton* button = new QPushButton;
-  button->setParent(this);
+  button->setParent( this);
   button->setText("下课");
 
+  //QPushButton的clicked信号不带参数，与teacherSingal信号不匹配，必须用不带参数的teacherSingal2
+  connect( button, &QPushButton::clicked, this->te, teacherSingal2);
+  //可以通过使用lambda表达式写槽函数
+  //connect( button, &QPushButton::clicked, this->te,[=](){
+  //    this->st->treat("西红柿炒蛋");
+  //  });
+  
   //信号和槽的拓展
   //1.信号连接信号
-  connect( button, &QPushButton::clicked, this->st, studentSlotNoStr);
   //2.一个信号可以连接多个槽函数
   //3.多个信号可以绑定同一个槽函数
+  //4.信号和槽的参数类型要一一对应
+  //5.信号的参数个数可以多余槽参数，反之不行，类型相同的参数个数也要一一对应
+  //6.可以使用 disconnect 断开信号槽的连接
+    //怎么连接的就要怎么断开
+
+  //Qt4版本的信号和槽的写法
+  //连接有参数的信号和槽
+  //优点：参数直观，写法简单
+  //缺点：编译器不检测参数是否符合，只会在运行后提示错误(最致命的缺点)
+  //connect(this->te, SIGNAL(hungry(QString)), this->st, SLOT(treat(QString)) );
 
   //下课函数调用
-//  classOver();
+  //classOver();
 }
 
 Widget::~Widget()
@@ -52,7 +69,7 @@ Widget::~Widget()
 void Widget::classOver(){
 
   //触发自定义信号
-  emit this->te->hungury();
+  emit this->te->hungry();
 
-  emit this->te->hungury("西红柿炒蛋");
+  emit this->te->hungry("西红柿炒蛋");
 }
